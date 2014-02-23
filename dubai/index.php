4d51a1437e6a037ce82078a32823906e9107812a
kolary <mengzhixiong@qq.com>
2014-02-23 14:49:49 +0800

    1
    1

--- dubai/index.php
+++ dubai/index.php
@@ -12,3 +12,10 @@ defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 
 require_once($yii);
 Yii::createWebApplication($config)->run();
+
+
+function echoToMobile($result)
+{
+	header('Content-type: application/json');
+	echo json_encode($result,JSON_UNESCAPED_UNICODE)
+}
