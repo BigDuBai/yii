a37b88bdb733bfe832a3a391c3ae25883813a07e
BigDuBai <floatbridge811@gmail.com>
2014-03-17 14:32:38 +0800

    no message

--- dubai/protected/controllers/VersionController.php
+++ dubai/protected/controllers/VersionController.php
@@ -23,4 +23,24 @@ class VersionController extends Controller
             echoStringToMobile($result->toArray()['versionNo']);
         }
     }
+
+    /**
+     * 获得更新的版本数据
+     * @param $versionNo
+     */
+    public function actionGetVersionData($versionNo) {
+        $resultArr = array();
+        $resultArr['version'] = $versionNo;
+        $result = Version::model()->find();
+        if(!empty($result))
+        {
+           $curv = $result->toArray()['versionNo'];
+           if ($curv>$versionNo) {
+               $resultArr['version'] = $curv;
+               for($index=$versionNo+1;$index<=$curv;$index++) {
+
+               }
+           }
+        }
+    }
 }
